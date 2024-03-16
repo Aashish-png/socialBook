dc18dffbc6ec25180c584bb05d9c2176fcc899d3
Aashish-png <aashishsarwa512@gmail.com>
2024-03-16 18:13:52 +0530

    home page saves likes done

--- src/lib/React-query/queriesAndMutation.ts
+++ src/lib/React-query/queriesAndMutation.ts
@@ -4,8 +4,21 @@ import {
   useInfiniteQuery,
   useQueryClient,
 } from "@tanstack/react-query";
-import { createUserAccount, signInAccount, signOutAccount } from "../appwrite/api";
-import { INewUser } from "@/types";
+import {
+  SavePost,
+  createPost,
+  createUserAccount,
+  deletedSavedPost,
+  getCurrentUser,
+  getRecentPosts,
+  likePost,
+  signInAccount,
+  signOutAccount,
+} from "../appwrite/api";
+import { INewPost, INewUser } from "@/types";
+import { QUERY_KEYS } from "./queryKeys";
+
+
 
 export const useCreateUserAccount = () => {
   return useMutation({
@@ -14,14 +27,131 @@ export const useCreateUserAccount = () => {
 };
 
 export const useSignInAccount = () => {
-    return useMutation({
-      mutationFn: (user: {email:string,password:string}) => signInAccount(user),
-    });
-  };
-
-  
-export const useSignOutAccount = () => {
   return useMutation({
-    mutationFn:  signOutAccount,
+    mutationFn: (user: { email: string; password: string }) =>
+      signInAccount(user),
   });
 };
+
+export const useSignOutAccount = () => {
+  return useMutation({
+    mutationFn: signOutAccount,
+  });
+};
+
+
+export const useCreatePost= ()=>{
+  const queryClient= useQueryClient();
+return useMutation(
+{
+mutationFn:(post:INewPost)=> createPost(post),
+onSuccess:()=>{
+  queryClient.invalidateQueries({
+
+    queryKey:[QUERY_KEYS.GET_RECENT_POSTS]
+  })
+
+
+}
+}
+)
+
+}
+
+export const useGetRecentPost=()=>{
+
+  return useQuery({
+
+
+    queryKey:[QUERY_KEYS.GET_RECENT_POSTS],
+
+    queryFn: getRecentPosts,
+  })
+}
+
+
+export const useLikePost= ()=>{
+
+  const queryClient= useQueryClient();
+
+    return useMutation({
+
+      mutationFn: ({postId, likesArray}:{postId:string; 
+        likesArray:string []})=>likePost(postId, likesArray),
+        onSuccess:(data)=>{
+          queryClient.invalidateQueries({
+            queryKey:[QUERY_KEYS.GET_POST_BY_ID, data?.$id]
+          })
+          queryClient.invalidateQueries({
+            queryKey:[QUERY_KEYS.GET_RECENT_POSTS]
+          })
+          queryClient.invalidateQueries({
+            queryKey:[QUERY_KEYS.GET_POSTS]
+          })
+          queryClient.invalidateQueries({
+            queryKey:[QUERY_KEYS.GET_CURRENT_USER]
+          })
+        }
+      
+    })
+
+}
+
+
+export const useSavePost= ()=>{
+
+  const queryClient= useQueryClient();
+
+    return useMutation({
+
+      mutationFn: ({postId, userId}:{postId:string; 
+        userId:string })=>SavePost(postId, userId),
+        onSuccess:()=>{
+          queryClient.invalidateQueries({
+            queryKey:[QUERY_KEYS.GET_RECENT_POSTS]
+          })
+          queryClient.invalidateQueries({
+            queryKey:[QUERY_KEYS.GET_POSTS]
+          })
+          queryClient.invalidateQueries({
+            queryKey:[QUERY_KEYS.GET_CURRENT_USER]
+          })
+        }
+      
+    })
+
+}
+
+
+export const useDeleteSavedPost= ()=>{
+
+  const queryClient= useQueryClient();
+
+    return useMutation({
+
+      mutationFn: (saveRecordId:string)=>deletedSavedPost(saveRecordId),
+        onSuccess:()=>{
+          queryClient.invalidateQueries({
+            queryKey:[QUERY_KEYS.GET_RECENT_POSTS]
+          })
+          queryClient.invalidateQueries({
+            queryKey:[QUERY_KEYS.GET_POSTS]
+          })
+          queryClient.invalidateQueries({
+            queryKey:[QUERY_KEYS.GET_CURRENT_USER]
+          })
+        }
+      
+    })
+
+}
+
+export const useGetCurrentUser= ()=>{
+  return useQuery(
+    {
+      queryKey:[QUERY_KEYS.GET_CURRENT_USER],
+      queryFn:getCurrentUser
+    }
+  )
+
+}
--- src/lib/appwrite/api.ts
+++ src/lib/appwrite/api.ts
@@ -1,5 +1,5 @@
-import { INewUser } from "@/types";
-import { account, appwriteConfig, avatars, database } from "./config";
+import { INewPost, INewUser } from "@/types";
+import { account, appwriteConfig, avatars, database, storage } from "./config";
 import { ID, Query } from "appwrite";
 
 export async function createUserAccount(user: INewUser) {
@@ -87,4 +87,165 @@ export async function signOutAccount() {
     console.log("error signning out  ", error)
   }
   
+}
+
+export async function createPost(post: INewPost) {
+  try {
+    //upload image to storage
+
+    const uploadedFile = await uploadFile(post.file[0]);
+
+    if (!uploadedFile) throw Error;
+
+    //get file url
+
+    const fileUrl = await getFilePreview(uploadedFile.$id);
+
+    if(!fileUrl){
+      deleteFile(uploadedFile.$id)
+      throw  Error;
+    }
+
+  //Convert tags
+    const tags=post.tags?.replace(/ /g, '').split(',')|| [];
+
+    const newPost = await database.createDocument(
+      appwriteConfig.databaseId,
+      appwriteConfig.postsCollectionId,
+      ID.unique(),
+      {
+        creator:post.userId,
+        caption:post.caption,
+        imageUrl:fileUrl,
+        imageId:uploadedFile.$id,
+        location:post.location,
+        tags:tags,
+      })
+
+      if(!newPost){
+        await deleteFile(uploadedFile.$id)
+        throw Error
+      }
+      return newPost 
+    
+  } catch (error) {
+    console.log("err", error);
+  }
+}
+
+export async function uploadFile(file: File) {
+  try {
+    const uploadedFile = await storage.createFile(
+      appwriteConfig.storageId,
+      ID.unique(),
+      file
+    );
+
+    return uploadedFile;
+  } catch (error) {}
+}
+
+export async function getFilePreview(fileId:string){
+
+try {
+  const fileUrl = storage.getFilePreview(
+    appwriteConfig.storageId,
+    fileId,
+    2000,
+    2000,
+    "top",
+    100,
+  )
+
+  return fileUrl
+} catch (error) {
+  console.log(error)
+}
+
+}
+
+export async function deleteFile(fileId:string){
+
+  try {
+    await storage.deleteFile(appwriteConfig.storageId, fileId);
+
+    return {status:'ok'}
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+
+export async function getRecentPosts(){
+  const posts= await database.listDocuments(
+    appwriteConfig.databaseId,
+    appwriteConfig.postsCollectionId,
+    [Query.orderDesc('$createdAt'), Query.limit(20)]
+  )
+  if(!posts) throw Error;
+  return posts
+}
+
+export async function likePost(postId:string,likesArray:string[]){
+
+  try {
+
+    const updatedPost = await database.updateDocument(
+      appwriteConfig.databaseId,
+      appwriteConfig.postsCollectionId,
+      postId,{
+        likes:likesArray
+      }
+    )
+
+    if(!updatedPost)throw Error
+
+    return updatedPost
+  } catch (error) {
+    console.log("error likes ",error)
+  }
+
+}
+
+export async function SavePost(postId:string,userId:string){
+
+  try {
+
+    const updatedPost = await database.createDocument(
+      appwriteConfig.databaseId,
+      appwriteConfig.savesCollectionId,
+      ID.unique(),
+      {
+       user:userId,
+       post:postId
+      }
+    )
+
+    if(!updatedPost)throw Error
+
+    return updatedPost
+  } catch (error) {
+    console.log("error likes ",error)
+  }
+
+}
+
+
+export async function deletedSavedPost(saveRecordId:string){
+
+  try {
+
+    const statusCode = await database.deleteDocument(
+      appwriteConfig.databaseId,
+      appwriteConfig.savesCollectionId,
+      saveRecordId,
+    )
+
+    if(!statusCode)throw Error
+
+    return {status:'ok'}
+  } catch (error) {
+    console.log("error likes ",error)
+  }
+
 }
