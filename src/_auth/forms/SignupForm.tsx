522763ba709091c6dae7d915dfc0021bb2c2f65e
Aashish-png <aashishsarwa512@gmail.com>
2024-04-23 19:08:19 +0530

    removed unused varibales

--- src/_auth/forms/SignupForm.tsx
+++ src/_auth/forms/SignupForm.tsx
@@ -58,7 +58,7 @@ export const SignupForm = () => {
       return toast({title:'sign in failed, Please try again.'})
     }
 
-    isUserLoading
+    
     const isLoggedIn= await checkAuthUser()
     if(isLoggedIn){
       form.reset();
@@ -133,7 +133,7 @@ export const SignupForm = () => {
           )}
         />
         <Button type="submit"  className='shad-button_primary'>
-          {isCreatingUser || isSigningIn ?(<div className='flex-center gap-2'>
+          {isCreatingUser || isSigningIn || isUserLoading ?(<div className='flex-center gap-2'>
              <Loader/> Loading...
           </div>):'Sign up '}
         </Button>
